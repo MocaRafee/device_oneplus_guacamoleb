aeb8df5be680da2d2dc242fe0bbb6dbeff4628c3
mocarafee <harif.rp05@gmail.com>
2020-06-08 02:07:54 +0700

    Guacamoleb: Bringup Bootleggers ten

--- bootleg_guacamoleb.mk
+++ bootleg_guacamoleb.mk
@@ -11,11 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamoleb device
 $(call inherit-product, device/oneplus/guacamoleb/device.mk)
 
-# Inherit some common Potato stuff.
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+# Include Bootleg common configuration
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+BOOTLEGGERS_BUILD_TYPE := Ryuukii
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2240
+TARGET_SCREEN_WIDTH := 1080
+PRODUCT_PACKAGES += \
+    SimIcons
+
+# Maintainer Prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bootleggers.maintainer=MocaRafee
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := potato_guacamoleb
+PRODUCT_NAME := bootleg_guacamoleb
 PRODUCT_DEVICE := guacamoleb
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 7
@@ -24,10 +34,6 @@ PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_AAPT_CONFIG := xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2240
-TARGET_SCREEN_WIDTH := 1080
 
 # Build info
 BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2003131900:user/release-keys"
